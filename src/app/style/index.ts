524f5d6b45ae90017f066d1b0f1be1faeb892f23
Daniel Toloto <dctoloto@gmail.com>
2024-03-11 18:56:18 -0300

    feat initial settings

---
+++ src/app/style/index.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background: ${({ theme }) => theme.colors.background};
+    font-family: Open-Sans, Helvetica, Sans-Serif;
+    font-weight: 700;
+    color: #fff;
+  }
+`
+
+export default GlobalStyle
---
+++ src/app/style/theme/index.ts
@@ -0,0 +1,7 @@
+export const defaultTheme = {
+  colors: {
+    primary: '#009EDD',
+    secondary: '#039B00',
+    background: '#2F2E41'
+  }
+}
