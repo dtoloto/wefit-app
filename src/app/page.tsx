1db90c0a6f55baff22d02267d227f78ba8a963dc
Daniel Toloto <dctoloto@gmail.com>
2024-03-11 19:41:33 -0300

    fix theme colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import { Container } from './atomic-components/atoms'
+import { Card } from './atomic-components/atoms/Card'
+import { Loader } from './atomic-components/atoms/Loader'
+
 export default function Home() {
-  return <main>Teste</main>
+  return (
+    <main>
+      <Container>Teste</Container>
+      <Loader />
+      <Card>Teste</Card>
+    </main>
+  )
 }
--- src/app/style/theme/index.ts
+++ src/app/style/theme/index.ts
@@ -2,6 +2,10 @@ export const defaultTheme = {
   colors: {
     primary: '#009EDD',
     secondary: '#039B00',
-    background: '#2F2E41'
-  }
+    background: '#2F2E41',
+    grey1: '#999999',
+    grey2: '#333333',
+    grey3: '#2F2E41'
+  },
+  borderRadius: '4px'
 }
